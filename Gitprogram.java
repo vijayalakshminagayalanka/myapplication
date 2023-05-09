5085804bb2f923dc0aec55d822af8de36ec7d67c
vijaya lakshmi <vijji3313@gmail.com>
2023-05-09 18:19:27 +0530

    helloworld2

---
+++ Gitprogram.java
@@ -0,0 +1,11 @@
+package gitproject1;
+
+public class Gitprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world ");
+         System.out.println("hi anusha");
+	}
+
+}
